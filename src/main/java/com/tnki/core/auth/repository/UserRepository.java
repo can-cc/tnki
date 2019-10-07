0bc7b362517d14fc742f10638e13ffc947a69d19
放為 <me@chenfangwei.com>
2019-10-07 21:26:19 +0800

    initial commit

--- src/main/java/com/tnki/core/auth/repository/UserRepository.java
+++ src/main/java/com/tnki/core/auth/repository/UserRepository.java
@@ -2,17 +2,22 @@ package com.tnki.core.auth.repository;
 
 import com.tnki.core.auth.model.User;
 import com.tnki.core.share.model.BaseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UserRepository extends BaseRepository {
 
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
     public User findByUsername(String username) {
         User user = new User();
         user.setID("U001");
-        user.setUsername("Linus");
-        user.setPassword("P123456");
+        user.setUsername("doge");
+        user.setPassword(bCryptPasswordEncoder.encode(("dddoge123")));
         return user;
     }
 
