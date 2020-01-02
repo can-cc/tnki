ee4d0a5aa75aa6df4652e9a30b7169183e38fa53
fwchen <me@chenfangwei.com>
2020-01-03 00:16:05 +0800

    change ann path

--- src/test/java/com/tnki/core/CoreApplicationTests.java
+++ src/test/java/com/tnki/core/CoreApplicationTests.java
@@ -1,6 +1,7 @@
 package com.tnki.core;
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
@@ -13,7 +14,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @DirtiesContext
 class CoreApplicationTest {
 
-	@org.junit.jupiter.api.Test
+	@Test
 	public void contextLoads() {
 		assertTrue(true);
 	}
--- src/test/java/com/tnki/core/memox/model/PeriodicCalculatorImplTest.java
+++ src/test/java/com/tnki/core/memox/model/PeriodicCalculatorImplTest.java
@@ -1,6 +1,7 @@
 package com.tnki.core.memox.model;
 
 import java.util.Date;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -8,7 +9,7 @@ class PeriodicCalculatorImplTest {
 
     private final PeriodicCalculator periodicCalculator = new PeriodicCalculatorImpl();
 
-    @org.junit.jupiter.api.Test
+    @Test
     void calcNextLearnDate() {
         MemoItem memoItem = new MemoItem();
         MemoLearningItem memoLearningItem = new MemoLearningItem(memoItem, 101);
@@ -18,7 +19,7 @@ class PeriodicCalculatorImplTest {
         assertEquals(nextLearnDate.toString(), MemoDateUtil.nextDays(1).toString());
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void calcNextEF() {
         double nextEF = periodicCalculator.calcNextEF(1.3, 5);
         assertEquals(nextEF, 1.308);
