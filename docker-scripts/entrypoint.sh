6ce02c62ebe2af5f40f4adbcc9ab54b7636807d1
fwchen <me@chenfangwei.com>
2020-03-04 10:20:07 +0800

    use entrypoint.sh start docker container

---
+++ docker-scripts/entrypoint.sh
@@ -0,0 +1,6 @@
+java -javaagent:./elastic-apm-agent-1.13.0.jar \
+     -Delastic.apm.service_name=$APM_APPLICATION_NAME \
+     -Delastic.apm.server_url=$APM_SERVER_URL \
+     -Delastic.apm.secret_token=$APM_SECRET_TOKEN \
+     -Delastic.apm.application_packages=com.tnki.core \
+     -jar /app/build/libs/core-latest.jar
