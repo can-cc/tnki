c157fef8d905fd46aaa8c43e31b6dcf810db1c01
fwchen <me@chenfangwei.com>
2020-03-30 17:22:06 +0800

    change default configure

--- src/main/java/com/tnki/core/CoreController.java
+++ src/main/java/com/tnki/core/CoreController.java
@@ -1,16 +1,17 @@
 package com.tnki.core;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class CoreController {
 
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String hello() {
+        log.info("hello world");
         return "Hello world";
     }
-
-
 }
